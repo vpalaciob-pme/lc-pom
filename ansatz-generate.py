e60f72511e6042f768f856766094c37f14fed1ce
Elise Chen <63219182+ECC-notacoder@users.noreply.github.com>
2023-04-03 22:09:19 -0500

    Adding in uniaxial and concentric modes.

--- ansatz-generate.py
+++ ansatz-generate.py
@@ -78,19 +78,107 @@ def radial (r,  dx = 0.1):
     nn[idx] = 0
 
     # return the parameters
-    consts = (l_box,nx,ny,nz,dx)
+    consts = (L,centroid, l_box,nx,ny,nz,dx)
+    return rr, nn, consts, idx
+
+# In[]:
+def concentric (r, dx = 0.1):
+    # Make sphere centered at origin
+    L = 2*r*np.ones(3);
+    centroid = np.asarray([0,0,0])
+    # Pad by 10%, round nx, ny, nz to be multiples of 10
+    l_box = L *1.1
+    l_box[2] = L[2]*1.01
+    nl = np.ceil(l_box/dx/10)*10
+    nl = np.asarray (nl, dtype= np.int32)
+    l_box = nl*dx
+
+    nx, ny, nz = nl
+    print ("nx, ny, nz", nx, ny, nz)
+    print ("total data points: ", nx*ny*nz)
+
+    # rr are the coordinates; nn are the director on the coordinate points
+    rr = np.zeros ((nx*ny*nz, 3))
+    nn = np.zeros ((nx*ny*nz, 3))
+
+    # create a mesh (fill in)
+    x = np.linspace(-l_box[0]/2, l_box[0]/2, nx)
+    y = np.linspace(-l_box[1]/2, l_box[1]/2, ny)
+    z = np.linspace(-l_box[2]/2, l_box[2]/2, nz)
+    xv, yv,zv = np.meshgrid(x, y, z,indexing='ij')
+    rr[:,0] = xv.flatten();rr[:,1] = yv.flatten();rr[:,2] = zv.flatten()
+    angles = np.arctan2(rr[:,1],rr[:,0])
+    # Normalize the directors and avoid 0/0 error
+    nn = np.copy(rr)
+    nn[:,0] = np.sin(angles)
+    nn[:,1] = -np.cos(angles)
+    nn[:,2] = 0
+
+    # identify which points are outside the sphere and set the director to zero
+    idx = np.where (ellip1(rr.T,L, centroid)>0)
+    print ("Points that are outside", idx)
+    print (len (np.asarray(idx[0])))
+    nn[idx] = 0
+
+    # return the parameters
+    consts = (L,centroid, l_box,nx,ny,nz,dx)
+    return rr, nn, consts
+# In[]:
+def uniaxial (r, angle, dx = 0.1):
+    # Make sphere centered at origin
+    L = 2*r*np.ones(3);
+    centroid = np.asarray([0,0,0])
+    # Pad by 10%, round nx, ny, nz to be multiples of 10
+    l_box = L *1.1
+    l_box[2] = L[2]*1.01
+    nl = np.ceil(l_box/dx/10)*10
+    nl = np.asarray (nl, dtype= np.int32)
+    l_box = nl*dx
+
+    nx, ny, nz = nl
+    print ("nx, ny, nz", nx, ny, nz)
+    print ("total data points: ", nx*ny*nz)
+
+    # rr are the coordinates; nn are the director on the coordinate points
+    rr = np.zeros ((nx*ny*nz, 3))
+    nn = np.zeros ((nx*ny*nz, 3))
+
+    # create a mesh (fill in)
+    x = np.linspace(-l_box[0]/2, l_box[0]/2, nx)
+    y = np.linspace(-l_box[1]/2, l_box[1]/2, ny)
+    z = np.linspace(-l_box[2]/2, l_box[2]/2, nz)
+    xv, yv,zv = np.meshgrid(x, y, z,indexing='ij')
+    rr[:,0] = xv.flatten();rr[:,1] = yv.flatten();rr[:,2] = zv.flatten()
+
+    # Normalize the directors and avoid 0/0 error
+    nn = np.copy(rr)
+    nn[:,0] = np.cos(angle)
+    nn[:,1] = np.sin(angle)
+    nn[:,2] = 0
+
+    # identify which points are outside the sphere and set the director to zero
+    idx = np.where (ellip1(rr.T,L, centroid)>0)
+    print ("Points that are outside", idx)
+    print (len (np.asarray(idx[0])))
+    nn[idx] = 0
+
+    # return the parameters
+    consts = (L,centroid, l_box,nx,ny,nz,dx)
     return rr, nn, consts
 
 
+# In[6]:
 # # Plot midplane to visualize
 
-# In[6]:
-
-
-def plot_mid(rr,nn,l_box,toplot = False):
-    idx2 = np.where (np.abs(rr[:,2]-0)<0.1*np.max(rr) )
-    rr2 = rr[idx2]; nn2 = nn[idx2]
-
+def plot_mid(rr,nn,consts, savename = None):
+    # Plot only the points inside the droplet
+    L,centroid, l_box,nx,ny,nz,dx = consts
+    idx = np.where (ellip1(rr.T,L, centroid)<0)
+    rr1 = rr[idx]; nn1 = nn[idx]
+    # Get the midplane
+    idx2 = np.where (np.abs(rr1[:,2]-0)<0.1*np.max(rr1) )
+    rr2 = rr1[idx2]; nn2 = nn1[idx2]
+    
     fig, ax = plt.subplots(figsize = (5,5))
     ax.quiver(rr2[:,0], rr2[:,1],  nn2[:,0], nn2[:,1], scale = 50, width = 1.0E-3,color = 'r', pivot = 'mid',headwidth =0, label = 'quintic')
 
@@ -101,18 +189,16 @@ def plot_mid(rr,nn,l_box,toplot = False):
     ax.set_xlim(-l_box[0]/2,l_box[0]/2)
 
     plt.tight_layout()
-    if (toplot):
-        plt.savefig("./Images/RadialField.png")
+    if (savename != None):
+        plt.savefig(savename)
     return
 
 
 # # Save as a txt file
 
 # In[7]:
-
-
 def save_txt(rr,nn, consts, fname):
-    l_box, nx,ny,nz,dx = consts
+    L,centroid, l_box,nx,ny,nz,dx = consts
     X0 = np.hstack([rr,nn])
     header = 'Radial; director file \n'
     info0 = "Firstline:\nNx"+"\t" + "Ny" +"\t"+ "Nz"+"\t" +"dx"+"\t" + "dy" +"\t"+ "dz"+"\n"
@@ -137,6 +223,7 @@ def replace_dot (num):
 
 # In[9]:
 if __name__ == "__main__":
+    plt.close("all")
 
     """
     rr, nn, consts = radial (10, dx = 0.1)
@@ -145,15 +232,58 @@ if __name__ == "__main__":
     fname = "Interpolated_Director_Field/10um-Radial-dx0p1.txt"
     save_txt(rr,nn, consts, fname)
     """
+    
+    # Radial size
+    
+    """
     directory = "Interpolated_Director_Field/"
     Rlist = np.asarray([10.5])
     dxlist = np.asarray([0.2])
     for R in Rlist:
         for dx in dxlist:
-            rr, nn, consts = radial (R, dx = dx)
+            rr, nn, consts, idx = radial (R, dx = dx)
             l_box, nx,ny,nz,dx = consts
-            #plot_mid(rr,nn,l_box,toplot = False)
+            #plot_mid(rr,nn,l_box,idx, savename = None)
             fname = replace_dot(R) +"um-Radial-"+replace_dot(dx) +".txt"
             print (fname)
             print (directory +fname)
             save_txt(rr,nn, consts, directory+fname)
+    """
+ # In[9]:   
+ 
+    
+    # Uniaxial quick test
+    directory = "Interpolated_Director_Field/"
+    Rlist = np.asarray([10.5])
+    dxlist = np.asarray([0.5])
+    anglelist = np.asarray([15,75,105,165])
+    R = 10.0; dx = 0.25
+    for angle in anglelist:
+        angle1 = angle/180*np.pi
+        rr, nn, consts = uniaxial (R, angle1, dx = dx)
+        L,centroid, l_box,nx,ny,nz,dx = consts
+        picname =  replace_dot(R) +"um-uniaxial-"+replace_dot(angle) +".png"
+        #plot_mid(rr,nn, consts, savename = picname)
+        fname = replace_dot(R) +"um-uniaxial-"+replace_dot(angle) +".txt"
+        print (fname)
+        print (directory +fname)
+        save_txt(rr,nn, consts, directory+fname)
+
+ # In[]:  
+    """
+    # Uniaxial quick test
+    directory = "Interpolated_Director_Field/"
+    Rlist = np.asarray([10.5])
+    dxlist = np.asarray([0.5])
+    anglelist = np.asarray([0, 45,90])
+    R = 10.0; dx = 0.5
+
+    rr, nn, consts = concentric (R, dx = dx)
+    L,centroid, l_box,nx,ny,nz,dx = consts
+    picname =  replace_dot(R) +"um-concentric" +".png"
+    plot_mid(rr,nn, consts, savename = picname)
+    fname = replace_dot(R) +"um-concentric" +".txt"
+    print (fname)
+    print (directory +fname)
+    #save_txt(rr,nn, consts, directory+fname)
+    """
